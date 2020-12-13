e4f7edaad33f5c2d1d21323d9d35259564438ef6
Cody Lee <cody.lee@datadoghq.com>
2020-12-13 09:09:48 -0600

    prepare day 14 boilerplate

---
+++ year_2020/puzzles/p_14/src/main.rs
@@ -0,0 +1,32 @@
+#[allow(unused_imports)]
+use util::*;
+
+fn main() {
+    println!("part 1 = {}", part_1("puzzles/p_14/data/input"));
+    println!("part 2 = {}", part_2("puzzles/p_14/data/input"));
+}
+
+fn part_1(_filename: &str) -> i64 {
+    0
+}
+
+fn part_2(_filename: &str) -> i128 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_part_1() {
+        let expected: i64 = 0;
+        let result: i64 = super::part_1("data/input");
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let expected: i128 = 0;
+        let result: i128 = super::part_2("data/input");
+        assert_eq!(expected, result);
+    }
+}
