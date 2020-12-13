c7e5ddac2e00c59bca242a250fd12160dc1000a7
Cody Lee <cody.lee@datadoghq.com>
2020-12-12 23:34:13 -0600

    day 13 part 1 solved, part 2 I am too sleepy lol

--- year_2020/puzzles/p_13/src/main.rs
+++ year_2020/puzzles/p_13/src/main.rs
@@ -6,12 +6,53 @@ fn main() {
     println!("part 2 = {}", part_2("puzzles/p_13/data/input"));
 }
 
-fn part_1(_filename: &str) -> i64 {
+fn part_1(filename: &str) -> i64 {
+    let (depart_time, bus_times) = read_raw_data(filename);
+    println!("depart_time={} bus_times={:?}", depart_time, bus_times);
+    let mut closest_bus_time_id: i64 = 0;
+    for bus_time in bus_times {
+        println!(
+            "bus_time={} closest_bus_time_id={}",
+            bus_time, closest_bus_time_id
+        );
+        if depart_time % bus_time == 0 {
+            closest_bus_time_id = bus_time;
+            break;
+        }
+        if closest_bus_time_id == 0 {
+            closest_bus_time_id = bus_time
+        } else if depart_time % bus_time > depart_time % closest_bus_time_id {
+            closest_bus_time_id = bus_time
+        }
+    }
+    let minutes_waiting = closest_bus_time_id - depart_time % closest_bus_time_id;
+    closest_bus_time_id * minutes_waiting
+}
+
+fn part_2(filename: &str) -> i64 {
+    let (_, bus_times) = read_raw_data(filename);
+
     0
 }
 
-fn part_2(_filename: &str) -> i64 {
-    0
+fn read_raw_data(filename: &str) -> (i64, Vec<i64>) {
+    let mut depart_time: i64 = 0;
+    let mut times: Vec<i64> = Vec::new();
+    if let Ok(lines) = util::iter_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        for (i, line) in lines.enumerate() {
+            if i == 0 {
+                depart_time = line.parse::<i64>().expect("invalid first line");
+            } else {
+                times = line
+                    .split(",")
+                    .filter(|&x| x != "x")
+                    .map(|x| x.parse::<i64>().expect("invalid integer"))
+                    .collect_vec()
+            }
+        }
+    }
+    (depart_time, times)
 }
 
 #[cfg(test)]
